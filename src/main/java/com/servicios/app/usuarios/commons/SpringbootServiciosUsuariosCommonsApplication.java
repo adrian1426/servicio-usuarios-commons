77f87734d8dd490fa7eab6bb986cd2ea30f01158
Adrian1426 <adrianhh1426@gmail.com>
2021-02-01 10:04:01 -0600

    proyecto inicial

---
+++ src/main/java/com/servicios/app/usuarios/commons/SpringbootServiciosUsuariosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.servicios.app.usuarios.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiciosUsuariosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiciosUsuariosCommonsApplication.class, args);
+	}
+
+}
